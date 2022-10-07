1e963380a3c4b5db0b9488486d5c5844c1b0f501
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-07 06:38:48 +0400

    example added

---
+++ example/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	im "github.com/eightlay/InfiniteMath"
+)
+
+func main() {
+	// Create matrix
+	m1 := im.NewMatrix([][]int{
+		{1, 2},
+		{3, 4},
+	})
+	m2 := im.NewMatrix([][]int{
+		{1, 0},
+		{0, 1},
+	})
+
+	// Add scalar
+	m1.AddScalar(1)
+	fmt.Printf("%s\n", m1)
+
+	// Multiply by scalar
+	m2.MultScalar(2)
+	fmt.Printf("%s\n", m2)
+
+	// Element-wise addition
+	m1.Add(m2)
+	fmt.Printf("%s\n", m1)
+
+	// Element-wise multiplication
+	m2.Mult(m1)
+	fmt.Printf("%s\n", m2)
+
+	// Dot product
+	m3 := im.Dot(m1, m2)
+	fmt.Printf("%s\n", m3)
+}
