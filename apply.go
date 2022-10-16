b0bba8d41ccc3bc6d220f12a5505d93780a43848
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-17 03:55:41 +0400

    operator to func changed

--- apply.go
+++ apply.go
@@ -5,20 +5,20 @@ import (
 )
 
 // Apply operator to each matrix element
-func Apply[T c.Numeric](m *Matrix[T], op Operator[T, T]) {
+func Apply[T c.Numeric](m *Matrix[T], f func(T) T) {
 	for row := uint(0); row < m.height; row++ {
 		for col := uint(0); col < m.width; col++ {
-			m.vals[row][col] = op(m.vals[row][col])
+			m.vals[row][col] = f(m.vals[row][col])
 		}
 	}
 }
 
 // Apply operator along axis
-func ApplyAlongAxis[Ti c.Numeric, To c.Numeric](m *Matrix[Ti], rowAxis bool, f func([]Ti) To) *Matrix[To] {
+func ApplyAlongAxis[Ti c.Numeric, To c.Numeric](m *Matrix[Ti], rowWise bool, f func([]Ti) To) *Matrix[To] {
 	var getFunc func(row, col uint) []Ti
 	var itRow, itCol uint
 
-	if rowAxis {
+	if rowWise {
 		getFunc = func(row, col uint) []Ti {
 			return m.Row(row).AsSlice()[0]
 		}
