5a51624047b1dcd2ddaf7b52e87fb38d408a8215
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-16 19:51:25 +0400

    Apply along axis added

--- apply.go
+++ apply.go
@@ -12,3 +12,31 @@ func Apply[T c.Numeric](m *Matrix[T], op Operator[T, T]) {
 		}
 	}
 }
+
+// Apply operator along axis
+func ApplyAlongAxis[Ti c.Numeric, To c.Numeric](m *Matrix[Ti], rowAxis bool, f func([]Ti) To) *Matrix[To] {
+	var getFunc func(row, col uint) []Ti
+	var itRow, itCol uint
+
+	if rowAxis {
+		getFunc = func(row, col uint) []Ti {
+			return m.Row(row).AsSlice()[0]
+		}
+		itRow, itCol = m.height, 1
+	} else {
+		getFunc = func(row, col uint) []Ti {
+			return m.Col(col).Transpose().AsSlice()[0]
+		}
+		itRow, itCol = 1, m.width
+	}
+
+	result := ZeroMatrix[To](itRow, itCol)
+
+	for row := uint(0); row < itRow; row++ {
+		for col := uint(0); col < itCol; col++ {
+			result.vals[row][col] = f(getFunc(row, col))
+		}
+	}
+
+	return result
+}
