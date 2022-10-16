0f66377b45684aee9eed2d1ce380bd1664a31766
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-17 03:14:50 +0400

    matrix test update

--- tests/matrix_test.go
+++ tests/matrix_test.go
@@ -22,3 +22,34 @@ func TestString(t *testing.T) {
 		t.Fatal("invalid matrix creation")
 	}
 }
+
+func TestCopy(t *testing.T) {
+	m := im.MatrixFromSlice([][]int{{1, 2}})
+	mc := m.Copy()
+
+	if !m.Equal(mc) {
+		t.Fatal("invalid matrix creation")
+	}
+}
+
+func TestAsSlice(t *testing.T) {
+	m := im.MatrixFromSlice([][]int{{1}, {2}})
+	s := m.AsSlice()
+
+	t1 := m.Get(0, 0) == s[0][0]
+	t2 := m.Get(1, 0) == s[1][0]
+
+	if !(t1 && t2) {
+		t.Fatal("invalid matrix creation")
+	}
+}
+
+func TestTranspose(t *testing.T) {
+	m := im.MatrixFromSlice([][]int{{1, 2}})
+	mc := im.MatrixFromSlice([][]int{{1}, {2}})
+	mt := m.Transpose()
+
+	if !mc.Equal(mt) {
+		t.Fatal("invalid matrix creation")
+	}
+}
