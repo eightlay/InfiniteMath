f1284933b74f89e7fba4f61d3e29a6e4555334c7
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-17 18:22:07 +0400

    WriteInner, WriteDot added

--- linalg.go
+++ linalg.go
@@ -8,3 +8,8 @@ import (
 func Dot[T c.Numeric](m1, m2 *Matrix[T]) *Matrix[T] {
 	return Inner(m1, m2, multOp[T], addOp[T])
 }
+
+// Dot product of two matrices
+func WriteDot[T c.Numeric](to, m1, m2 *Matrix[T]) {
+	WriteInner(to, m1, m2, multOp[T], addOp[T])
+}
